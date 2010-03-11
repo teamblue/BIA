5fa43a3bb584b02a0aa65cf9f5da7f3a13807474
Samuel Pauls <samuelpauls@gmail.com>
2010-03-11 16:19:30 -0600

    New test for server/client two way socket communication.

--- BTProxy/src/tests/LibraryTests.java
+++ BTProxy/src/tests/LibraryTests.java
@@ -1,12 +1,22 @@
 package tests;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import junit.framework.TestCase;
 
 /**
- * Tests the libraries that are used by the Bluetooth internet adapter desktop
+ * Tests the libraries that are used by the Bluetooth Internet adapter desktop
  * client.
+ * 
+ * Testing these libraries is useful as a form of documentation for how this
+ * program interacts with the system underneath and in the case where an
+ * implementation of the libraries on a given OS may have issues.
  * 
  * @author Samuel Pauls
  */
@@ -36,7 +46,53 @@ public class LibraryTests extends TestCase {
 			message = message.substring(0, message.indexOf('\r'));
 			assertEquals (message, "HTTP/1.1 200 OK");
 		} catch (IOException e) {
-			fail("Unable to connect to remote host " + HOST + " on port " + PORT);
+			fail("Unable to connect to remote host " + HOST + " on port " +
+			     PORT + "!");
+		}
+	}
+	
+	// Creates server and client sockets and attempts to send messages in both
+	// directions.
+	public void testSockets() {
+		final String HOST = "127.0.0.1";
+		final int PORT = 3128;
+		final String MESSAGE_TO_SERVER = "message to server\n";
+		final String MESSAGE_TO_CLIENT = "message to client\n";
+		
+		try {
+			// Create a server socket.
+			ServerSocket serverSocket = new ServerSocket(PORT);
+			
+			// Send a request to the server.
+			byte[] buffer = new byte[2048];
+			buffer = MESSAGE_TO_SERVER.getBytes();
+			Socket client = new Socket(HOST, PORT);
+			client.getOutputStream().write(buffer);
+			
+			// Accept the request on the server end.
+			Socket serverConnection = serverSocket.accept();
+			
+			// Verify the request.
+			InputStream is = serverConnection.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String receivedMessage = br.readLine();
+			assertEquals(receivedMessage + '\n', MESSAGE_TO_SERVER);
+			
+			// Send a message from the server to the client.
+			OutputStream os = serverConnection.getOutputStream();
+			PrintWriter pw = new PrintWriter(os);
+			pw.write(MESSAGE_TO_CLIENT);
+			pw.flush();
+			
+			// Receive the message on the client end and verify it.
+			InputStream clientIS = client.getInputStream();
+			InputStreamReader clientISR = new InputStreamReader(clientIS);
+			BufferedReader clientBR = new BufferedReader(clientISR);
+			receivedMessage = clientBR.readLine();
+			assertEquals(receivedMessage + '\n', MESSAGE_TO_CLIENT);
+		} catch(IOException e) {
+			fail("Unable to create socket on port " + PORT + "!");
 		}
 	}
 }
