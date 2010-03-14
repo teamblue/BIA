9d8d5753441b06ffc66733972b43a7321f02c00e
Aaron Petkau <aaron.petkau@gmail.com>
2010-03-14 15:34:20 -0500

    Got a basic version of TestServer working.  Only receives a request, and prints it out for now.

--- BIATestServer/src/testServer/TestServer.java
+++ BIATestServer/src/testServer/TestServer.java
@@ -1,27 +1,36 @@
 package testServer;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * A server running on the local machine, used to test out connections.
  * Should be run with regular java
- * Should be run before running other tests.
+ * Should be run before running other tests for mobile application.
  * @author aaron
  *
  */
 public class TestServer
-{
-	private static final String HTTP_NEWLINE = "\r\n";
+{	
+	private static String getHTTPNewline()
+	{
+		return "\r\n";
+	}
 	
-	public static final int serverPort = 8741;
+	private static int getServerPort()
+	{
+		return 8741;
+	}
 	
-	public static String getRequestMessage1()
+	public static String getRequestMessage()
 	{
 		String message =
-			"GET / HTTP/1.1" + HTTP_NEWLINE +
-			"Host: localhost" + HTTP_NEWLINE +
-			HTTP_NEWLINE;
+			"GET / HTTP/1.1" + getHTTPNewline() +
+			"Host: localhost" + getHTTPNewline() +
+			getHTTPNewline();
 		
 		return message;
 	}
@@ -29,18 +38,45 @@ public class TestServer
 	public static String getExpectedResponse()
 	{
 		String response =
-			"" + HTTP_NEWLINE +
-			"HTTP/1.1 200 OK" + HTTP_NEWLINE +
-			HTTP_NEWLINE;
+			getHTTPNewline() +
+			"HTTP/1.1 200 OK" + getHTTPNewline() +
+			getHTTPNewline();
 		
 		return response;
 	}
 	
+	private static String readRequest(InputStream is) throws IOException
+	{
+		int data;
+		StringBuffer request = new StringBuffer();
+		
+		while ((data = is.read()) != -1)
+		{
+			request.append((char)data);
+		}
+		
+		return request.toString();
+	}
+	
 	public static void main(String[] args)
 	{
+		ServerSocket server = null;
+		
+		String request = getRequestMessage();
+		String response = getExpectedResponse();
 		try
 		{
-			ServerSocket server = new ServerSocket(serverPort);
+			server = new ServerSocket(getServerPort());
+			System.out.println("Server running on " + server.getInetAddress() + ":" + server.getLocalPort());
+			
+			Socket clientSocket = server.accept(); // waits for connection
+			InputStream is = clientSocket.getInputStream();
+			OutputStream os = clientSocket.getOutputStream();
+			
+			request = readRequest(is);
+			System.out.println(request);
+			
+			server.close();
 		} 
 		catch (IOException e)
 		{
