7a64195d48c42275f89d2432996898f1e7de1984
wayne <wayne@wayne-desktop>
2010-03-21 17:27:37 -0500

    Implemented RemoteConnectionHandler.performTCP(), which takes a request as a byte array. It established a TCP connection, sends the request, and returns the response as a byte array.

--- BIAMobile/src/biamobile/RemoteConnectionHandler.java
+++ BIAMobile/src/biamobile/RemoteConnectionHandler.java
@@ -1,16 +1,54 @@
 package biamobile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.SocketConnection;
+
 public class RemoteConnectionHandler
 {
 	/**
-	 * Performs a connection using TCP to the remote host.
+	 * Performs a connection using TCP to the remote host. Returns the response as a byte array.
 	 * @param hostname  The host to connect to.
+	 * @param port The port on which to establish a connection.
 	 * @param request  The data for the request.
 	 * @return  The response data.
+	 * @throws IOException 
 	 */
-	public byte[] performConnectTCP(String hostname, byte[] request)
+	public byte[] performConnectTCP(String hostname, int port, byte[] request) throws IOException
 	{
-		return null;
+		//open raw TCP connection
+		SocketConnection client = (SocketConnection) Connector.open("socket://" + hostname + ":" + port);
+		//grab the streams
+		InputStream is = client.openInputStream();
+		OutputStream os = client.openOutputStream();
+		
+		//send the request
+		//System.out.println("Connected. Sending request...");
+		os.write(request);
+		os.flush();
+		
+		//parse the response using a StringBuffer (it's ugly, but it works...)
+		//System.out.println("Recieving response...");
+		StringBuffer responseStr = new StringBuffer("");
+		
+		int c = is.read();
+		while(c != -1)
+		{
+			responseStr.append((char) c);
+			c = is.read();
+		}
+		
+		//System.out.println(responseStr.toString());
+		
+		//close
+		//System.out.println("Closing connection...");
+		client.close();
+		//System.out.println("Connection closed.");
+		
+		return responseStr.toString().getBytes();
 	}
 	
 	public void performConnectHTTP(String[][] headers, String hostname, byte[] request)
