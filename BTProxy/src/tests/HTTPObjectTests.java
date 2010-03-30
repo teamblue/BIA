30ad693d729b3bba39a285f7b049913fdd76e6ac
Samuel Pauls <samuel@aires.(none)>
2010-03-30 14:14:16 -0500

    Cleaned up a bit of code.  Removed author names (mostly mine) because we worked on this project as a team. Simplified the HTTPObject.

--- BTProxy/src/tests/HTTPObjectTests.java
+++ BTProxy/src/tests/HTTPObjectTests.java
@@ -1,21 +1,18 @@
 package tests;
 
-import logic.HTTPObject;
+import logic.HTTPHeader;
 import junit.framework.TestCase;
 
 /**
  * Tests the HTTP object's ability to parse information out of a stream.
- * 
- * @author Samuel Pauls
  */
 public class HTTPObjectTests extends TestCase {
 	public void testGetHost() {
 		final String HOST = "www.google.ca";
-		final String REQUEST = "GET / HTTP/1.1\r\n"
-		                     + "Host: " + HOST;
-		
-		HTTPObject http = new HTTPObject(REQUEST);
-		
-		assertEquals (HOST, http.getHost());
+		final String REQUEST = "GET / HTTP/1.1\r\n" + "Host: " + HOST;
+
+		HTTPHeader http = new HTTPHeader(REQUEST);
+
+		assertEquals(HOST, http.getHost());
 	}
 }
