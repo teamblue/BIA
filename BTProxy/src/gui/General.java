364f9a60783782b183cece973b35038a34c3593c
Stefan Penner <stefan.penner@gmail.com>
2010-04-02 00:24:26 -0500

    Session Details no longer needed in gui/General.java Main deals with this announcement

--- BTProxy/src/gui/General.java
+++ BTProxy/src/gui/General.java
@@ -3,8 +3,6 @@ package gui;
 import javax.swing.JOptionPane;
 
 import logic.Main;
-import modal.SessionDetails;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.SWTError;
 import org.eclipse.swt.graphics.Image;
